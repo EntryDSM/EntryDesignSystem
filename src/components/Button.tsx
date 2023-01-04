7368bf64fecdf0488d71fa4161b56d09966e6c81
kimuichan <leerabi577@gmail.com>
2023-01-04 13:54:03 +0900

    chore: ts config setting

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,8 +5,6 @@ interface ButtonProps {}
 /**
  * Primary UI component for user interaction
  */
-function Button({ children }: PropsWithChildren<ButtonProps>) {
+export const Button = ({ children }: PropsWithChildren<ButtonProps>) => {
     return <button type="button">{children}</button>;
-}
-
-export default Button;
+};
