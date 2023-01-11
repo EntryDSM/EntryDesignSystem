ee25a9c587aac01bb411a5e174c93bdcaa013677
Kim Jeong Hyun <kimjh11130@naver.com>
2023-01-11 11:21:40 +0900

    feat: checkbox 개발

---
+++ src/stories/Checkbox.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Checkbox } from 'components/Checkbox';
+
+export default {
+    title: 'Component/Checkbox',
+    component: Checkbox,
+} as ComponentMeta<typeof Checkbox>;
+
+const Template: ComponentStory<typeof Checkbox> = (args) => <Checkbox {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+    color: 'orange',
+};
