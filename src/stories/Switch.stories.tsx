de0ffa17309c23e1a8b92c7e74f66f7431ee472f
Kim Jeong Hyun <kimjh11130@naver.com>
2023-01-11 11:26:10 +0900

    feat: switch 개발

---
+++ src/stories/Switch.stories.tsx
@@ -0,0 +1,11 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Switch } from 'components/Switch';
+
+export default {
+    title: 'Component/Switch',
+    component: Switch,
+} as ComponentMeta<typeof Switch>;
+
+const Template: ComponentStory<typeof Switch> = (args) => <Switch {...args} />;
+
+export const Primary = Template.bind({});
