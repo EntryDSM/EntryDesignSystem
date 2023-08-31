24374f5791704a2dc27ecaa81fe440b5268da5d7
jikwan <jikwan0327@gmail.com>
2023-08-31 15:34:47 +0900

    fix: textarea 버그 수정

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -27,7 +27,7 @@ export const Textarea: React.FC<InputProps> = ({
 }) => {
     const [totalText, setTotalText] = useState<number>(0);
     const totalOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setTotalText(value.length);
+        setTotalText(value?.length);
         onChange(e);
     };
     return (
